c391e594f3a7a7233122d2be5092c23b4a2cb183
Vladimir Vassilev <vladimir@lightside-instruments.com>
2023-02-01 05:06:44 +0100

    Release ice4pi-example-pll-1.0

--- rot.v
+++ rot.v
@@ -1,8 +1,9 @@
 
-module rot(input clk, output reg [11:0] rot);
+module rot(input clk, output D1, output D2, output D3, output D4, output D5);
    
    reg ready = 0;
    reg [23:0]     divider;
+   reg [3:0]      rot;
    
    always @(posedge clk) begin
       if (ready) 
@@ -10,7 +11,7 @@ module rot(input clk, output reg [11:0] rot);
            if (divider == 12000000) 
              begin
                 divider <= 0;
-                rot <= {rot[10:0], rot[11]};
+                rot <= {rot[2:0], rot[3]};
              end
            else 
              divider <= divider + 1;
@@ -18,15 +19,14 @@ module rot(input clk, output reg [11:0] rot);
       else 
         begin
            ready <= 1;
-           rot <= 12'b111111111110;
+           rot <= 4'b1110;
            divider <= 0;
         end
    end
    
-   //assign D1 = rot[0];
-   //assign D2 = rot[1];
-   //assign D3 = rot[2];
-   //assign D4 = rot[3];
-   //assign D5 = 1;
-
+   assign D1 = rot[0];
+   assign D2 = rot[1];
+   assign D3 = rot[2];
+   assign D4 = rot[3];
+   assign D5 = 1;
 endmodule // top
--- top.v
+++ top.v
@@ -7,32 +7,48 @@ module top(input clk,
            output reg [4:0] led,
            input [7:0] pmod);
 
-   reg rst = 1'b1;
-   reg [7:0] pmod_i2c;
-   wire [7:0] led_i2c;
-   reg [27:0] startup_cnt = 28'd0;
+    SB_PLL40_CORE #(
 
+/**
+ * PLL configuration
+ * user@xps:~/icestorm/icepll$ ./icepll -i 12 -o 120 -f pll.v -m
+ *
+ * F_PLLIN:    12.000 MHz (given)
+ * F_PLLOUT:  120.000 MHz (requested)
+ * F_PLLOUT:  120.000 MHz (achieved)
+ *
+ * FEEDBACK: SIMPLE
+ * F_PFD:   12.000 MHz
+ * F_VCO:  960.000 MHz
+ *
+ * DIVR:  0 (4'b0000)
+ * DIVF: 79 (7'b1001111)
+ * DIVQ:  3 (3'b011)
+ *
+ * FILTER_RANGE: 1 (3'b001)
+ *
+ */
 
-   i2cSlaveTop i2c0(.clk(clk), .rst(rst), .sda(i2c_sda), .scl(i2c_scl),
-                    //output regs 0-3
-                    .myReg0(led_i2c),
-                    //input regs 4-7
-                    .myReg4({pmod, 2'b00}),
+.FEEDBACK_PATH("SIMPLE"),
+.DIVR(4'b0000),		// DIVR =  0
+.DIVF(7'b1001111),	// DIVF = 79
+.DIVQ(3'b011),		// DIVQ =  3
+.FILTER_RANGE(3'b001)	// FILTER_RANGE = 1
+
+) uut(
+.REFERENCECLK   (clk),
+.PLLOUTGLOBAL   (clk_120mhz),
+.BYPASS         (bypass),
+.RESETB         (resetb)
 );
 
-   always @ (posedge clk)
-   begin
-      if(startup_cnt < 28'd144000000)
-        begin
-            rst <= 1'b1;
-            startup_cnt <= startup_cnt + 1;
-         end
-      else
-        begin
-            rst <= 1'b0;
-            led <= led_i2c[5:0];
-            pmod_i2c <= pmod[7:0];
-        end
-   end
+
+    wire clk_120mhz;
+    wire bypass;
+    wire resetb;
+    assign bypass = 0;
+    assign resetb = 1;
+
+    rot rot0(.clk(clk_120mhz), .D1(led[0]), .D2(led[1]), .D3(led[2]), .D4(led[3]), .D5(led[4]));
 
 endmodule // top
