fb26da9f65d94a2bcd4995fdf53b7fa49a5f2942
Vladimir Vassilev <vladimir@lightside-instruments.com>
2024-05-09 23:59:58 +0200

    Release ice4pi-example-ws2812b-1.2

--- top.v
+++ top.v
@@ -1,16 +1,19 @@
-//https://cdn-shop.adafruit.com/datasheets/WS2812B.pdf
+//https://cdn-shop.adafruit.com/datasheets/WS2811B.pdf
 
 module top(input clk, output sig1);
    
-   localparam LED_COUNT=24;
+   localparam LED_COUNT=12;
 
+                       //  b------Br------Rg------G
+   reg [23:0] value =  24'b000000010000000000000000;
    reg ready = 0;
    reg [23:0]     divider;
    reg [3:0] state0_counter;
    reg [3:0] state1_counter;
    reg ws2812b_data;
    reg [4:0] state;
- 
+   reg [5:0] bit_count = 0; 
+
    always @(posedge clk) begin
       if (ready) 
         begin
@@ -31,12 +34,12 @@ module top(input clk, output sig1);
    always @(posedge clk) begin
       if (ready) 
         begin
-          if (divider == (LED_COUNT*3*8*15-1))
+          if (divider == (LED_COUNT*3*8*15))
             begin
               state <= 2;
-              ws2812b_data <= 1;
+              ws2812b_data <= 0;
             end
-          else if (divider == (12000000-100*12-1))
+          else if (divider == (12000000-100*12))
             begin
               state <= 3;
               ws2812b_data <= 0;
@@ -44,36 +47,42 @@ module top(input clk, output sig1);
           else if (divider == (12000000-1)) 
             begin
               state <= 0;
-              ws2812b_data <= 0;
               state0_counter <= 0;
+              bit_count <= 0;
+              ws2812b_data <= 1;
             end
           else if(state == 0)
             begin
-             state0_counter <= state0_counter + 1;
-             if(state0_counter == 4)
-               begin
-                 state <= 1;
-                 state1_counter <= 0;
-                 ws2812b_data <= 1;
-               end
+              state0_counter <= state0_counter + 1;
+              if((state0_counter == 9 && value[bit_count] == 1) || (state0_counter == 4 && value[bit_count] == 0))
+                begin
+                  state <= 1;
+                  state1_counter <= 0;
+                  ws2812b_data <= 0;
+                end
             end
           else if(state == 1)
             begin
               state1_counter <= state1_counter + 1;
-              if(state1_counter == 9)
+              if((state1_counter == 4 && value[bit_count] == 1) || (state1_counter == 9 && value[bit_count] == 0))
                 begin
                   state <= 0;
                   state0_counter <= 0;
-                  ws2812b_data <= 0;
+                  ws2812b_data <= 1;
+                  if(bit_count == 23)
+                      bit_count <= 0;
+                  else
+                      bit_count <= bit_count + 1;
                 end
             end
         end
       else 
         begin
              state <= 0; 
-             ws2812b_data <= 0;
+             ws2812b_data <= 1;
              state0_counter <= 0;
              state1_counter <= 0;
+             bit_count <= 0;
         end
    end
    
