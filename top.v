67de155eff23e1d21fe3ba3eb3a075d62cdca701
Vladimir Vassilev <vladimir@lightside-instruments.com>
2024-05-10 02:55:56 +0200

    Release ice4pi-example-i2cslave-ws2812b-1.3

--- top.v
+++ top.v
@@ -5,20 +5,31 @@ module top(input clk,
            inout i2c_sda, input i2c_scl,
 
            output reg [4:0] led,
-           input [7:0] pmod);
+           input [7:0] pmod,
+           output sig1,
+);
 
    reg rst = 1'b1;
    reg [7:0] pmod_i2c;
    wire [7:0] led_i2c;
    reg [27:0] startup_cnt = 28'd0;
+   reg [7:0] red;
+   reg [7:0] green;
+   reg [7:0] blue;
+
 
 
    i2cSlaveTop i2c0(.clk(clk), .rst(rst), .sda(i2c_sda), .scl(i2c_scl),
                     //output regs 0-3
                     .myReg0(led_i2c),
+                    .myReg1(red),
+                    .myReg2(green),
+                    .myReg3(blue),
                     //input regs 4-7
                     .myReg4({pmod, 2'b00}),
 );
+
+   ws2812b ws2812b0(.clk(clk), .sig1(sig1), .value({red,green,blue}));
 
    always @ (posedge clk)
    begin
